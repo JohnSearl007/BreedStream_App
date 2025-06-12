005fea81ceadad6e19dd483f4bde792ab60d04d9
John Searl <jsearl007@gmail.com>
2025-06-12 09:19:11 -0500

    Initial Build
    Central Shiny App

---
+++ app.R
@@ -0,0 +1,107 @@
+library(shiny)
+library(data.table)
+library(dplyr)
+library(bigmemory)
+library(BreedStream)
+library(StageWise)
+library(ibdsim2)
+library(shinyWidgets)
+library(shinyFiles)
+
+# Source module files (adjust paths as needed)
+source("~/Documents/BreedStream_App/BreedStream_Panels/Cleaning.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/Format.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/insilico.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/TwoStage.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/Index.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/Effects.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/UC.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/OMA.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/TransposedPredict.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/Transposedinsilico.R")
+source("~/Documents/BreedStream_App/BreedStream_Panels/QualityCheck.R")
+
+# Set max upload size to 60GB
+options(shiny.maxRequestSize = 60000 * 1024^2)
+
+# Main UI
+ui <- navbarPage(
+  title = "BreedStream Shiny App",
+  tabPanel(
+    "Getting Started",
+    sidebarLayout(
+      sidebarPanel(
+        shinyDirButton("workdir", "Select Working Directory", "Choose a directory for output files"),
+        verbatimTextOutput("workdir_path")
+      ),
+      mainPanel(
+        h4("Select a working directory to store output files, such as prediction results.")
+      )
+    )
+  ),
+  navbarMenu(
+    "Clean & Format",
+    tabPanel("Clean", cleanUI("clean")),
+    tabPanel("Format", formatUI("format")),
+    tabPanel("Quality Check", qualityUI("quality"))
+  ),
+  tabPanel("in silico Hybrids", insilicoUI("insilico")),
+  navbarMenu(
+    "Analysis",
+    tabPanel("Model", twoStageUI("model")),
+    tabPanel("Index", indexUI("index")),
+    tabPanel("Marker Effects", effectsUI("effects"))
+  ),
+  navbarMenu(
+    "Breeding Crosses",
+    tabPanel("Usefulness Criterion", ucUI("uc")),
+    tabPanel("Optimal Mate Allocation", omaUI("oma"))
+  ),
+  navbarMenu(
+    "RR-BLUP Prediction",
+    tabPanel("Transpose", insilico_transposedUI("insilico_transposed")),
+    tabPanel("Prediction", predictUI("predict"))
+  )
+)
+
+# Main Server
+server <- function(input, output, session) {
+  # Define directory chooser
+  volumes <- c(Home = fs::path_home(), getVolumes()())
+  shinyDirChoose(input, "workdir", roots = volumes, session = session)
+  
+  # Reactive value to store selected directory
+  workdir <- reactive({
+    if (is.integer(input$workdir)) {
+      return(getwd())
+    }
+    parseDirPath(volumes, input$workdir)
+  })
+  
+  # Display selected directory
+  output$workdir_path <- renderText({
+    workdir()
+  })
+  
+  # Call modules, passing workdir to all modules
+  callModule(cleanServer, "clean", workdir = workdir)
+  callModule(formatServer, "format", workdir = workdir)
+  callModule(insilicoServer, "insilico", workdir = workdir)
+  callModule(twoStageServer, "model", workdir = workdir)
+  callModule(indexServer, "index", workdir = workdir)
+  callModule(effectsServer, "effects", workdir = workdir)
+  callModule(ucServer, "uc", workdir = workdir)
+  callModule(omaServer, "oma", workdir = workdir)
+  callModule(predictServer, "predict", workdir = workdir)
+  callModule(insilico_transposedServer, "insilico_transposed", workdir = workdir)
+  callModule(qualityServer, "quality", workdir = workdir)
+  
+  # Clean up temporary files on session end
+  session$onSessionEnded(function() {
+    temp_files <- list.files(tempdir(), full.names = TRUE)
+    if (length(temp_files) > 0) unlink(temp_files)
+  })
+}
+
+# Run the app
+shinyApp(ui, server)
